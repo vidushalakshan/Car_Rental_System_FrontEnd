ff7b71e4f9cae636576578d3c02f230f839296dc
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2022-07-23 22:05:57 +0530

    aded driver text field

--- src/assets/script/script.js
+++ src/assets/script/script.js
@@ -1,12 +1,21 @@
-$("#dashboard").css("display", "none");
+$("#dashboard").css("display", "");
 $("#admin").css("display", "none");
+$("#vidusha").css("display", "none");
 
 $("#dashboardlink").click(function () {
-    $("#dashboard").css("display", "");
+    $("#dashboard").css("display", "none");
     $("#admin").css("display", "none");
+    $("#driver").css("display", "block");
 });
 
 $("#adminlink").click(function () {
     $("#dashboard").css("display", "none");
     $("#admin").css("display", "block");
+    $("#driver").css("display", "none");
+});
+
+$("#driverlink").click(function () {
+    $("#dashboard").css("display", "");
+    $("#admin").css("display", "none");
+    $("#driver").css("display", "none");
 });
